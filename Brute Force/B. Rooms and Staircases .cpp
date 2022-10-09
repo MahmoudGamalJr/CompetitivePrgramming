3e43cbd1c610203106717fea78f4c06a11a6ddcf
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-10-09 13:49:56 +0200

    Update B. Rooms and Staircases .cpp

--- Brute Force/B. Rooms and Staircases .cpp
+++ Brute Force/B. Rooms and Staircases .cpp
@@ -14,24 +14,24 @@ using namespace std;
 int main()
 {
 
-        int Test ; cin >> Test;
+        int Test ; cin >> Test; // test cases
 
         while(Test--)
         {
 
-            int roomSize;
-            string room;
+            int roomSize; // the size of the string
+            string room; //the string
             cin >> roomSize >> room;
             bool is_one_founded = false, first = true, last = true;
             int firstOne = 0, lastOne = 0, decreising = roomSize - 1;
-            if(room[0]=='1' || room[roomSize-1] == '1')
+            if(room[0]=='1' || room[roomSize-1] == '1') // if the last index or the first is '1'
             {
                 cout << roomSize * 2 << '\n';
                 continue;
             }
             for(int i = 0; i < roomSize;i++)
             {
-                is_one_founded |=(room[i]=='1');
+                is_one_founded |=(room[i]=='1'); // AND opertor to check if there are no ones in the string
 
                 if(room[i]=='1' && first)
                 {
@@ -46,10 +46,10 @@ int main()
                 }
             }
 
-            firstOne = roomSize - firstOne;
+            firstOne = roomSize - firstOne; // the first '1' index - the total size
             lastOne = roomSize - lastOne;
             firstOne*=2, lastOne*=2;
-            cout << ( is_one_founded ? max(firstOne, lastOne) : roomSize) << '\n';
+            cout << ( is_one_founded ? max(firstOne, lastOne) : roomSize) << '\n'; // ternary operator if check if there is '1' in the string or NOT
 
         }
 
