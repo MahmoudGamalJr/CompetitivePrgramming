57c77f845ff51b97a97cedba7ebe1c5aead45719
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-10-12 17:23:20 +0200

    Operator , *900

---
+++ Operator/A - Iroha and Haiku .c
@@ -0,0 +1,34 @@
+// Problem Name: A - Iroha and Haiku
+// Problem Link: https://atcoder.jp/contests/abc042/tasks/abc042_a
+// Language: C
+// Submission Link: https://atcoder.jp/contests/abc042/submissions/35610156
+// User: MahmoudGamal
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+
+
+        int a , b , c;
+        scanf("%d %d %d", &a, &b, &c);
+        if(a + b + c == 17) // if their sum = 17 then its YES otherwise NO
+        {
+            printf("YES");
+        }
+        else
+        {
+            printf("NO");
+        }
+
+
+
+
+
+
+       return 0;
+}
