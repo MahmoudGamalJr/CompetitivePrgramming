3fd42e4136d36d1b47c587ee7015d0e468032582
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-10-12 18:09:58 +0200

    string , *1100

---
+++ Strings/B - Beautiful Strings .cpp
@@ -0,0 +1,44 @@
+// Problem Name: B - Beautiful Strings
+// Problem Link: https://atcoder.jp/contests/abc044/tasks/abc044_b
+// Language: CPP
+// Submission Link: https://atcoder.jp/contests/abc044/submissions/35610759
+// User: MahmoudGamal
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+        ios_base :: sync_with_stdio(0),
+        cin.tie(0);
+
+        string str; cin >> str;
+        
+        vector <int> alph(26);
+        
+        for(int i = 0; i  < (int)str.size();i++)
+        {
+            alph[str[i]-'a']++;
+        }
+        for(int i = 0; i < 26;i++)
+        {
+            if(alph[i]%2!=0)
+            {
+                cout << "No";  // frequency technique
+                return 0;
+            }
+        }
+cout << "Yes";
+
+
+
+
+
+
+
+
+
+	 return 0;
+
+}
