588619891ae2accd05458b9af711fbf839d9a7f4
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-10-12 18:19:21 +0200

    math, *800

---
+++ Mathematics/A - Children and Candies .c
@@ -0,0 +1,29 @@
+// Problem Name: A - Children and Candies
+// Problem Link: https://atcoder.jp/contests/abc043/tasks/abc043_a
+// Language: C
+// Submission Link: https://atcoder.jp/contests/abc043/submissions/35610275
+// User: MahmoudGamal
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+
+
+        int sum, ans;
+        scanf("%d", &sum);
+        ans = sum * (sum+1) / 2;  // sum of 1 to the number
+
+        printf("%d", ans);
+
+
+
+
+
+
+       return 0;
+}
