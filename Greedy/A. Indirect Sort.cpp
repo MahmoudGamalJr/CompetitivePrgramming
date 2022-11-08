18f841918a309042059001dfa6e539243395b425
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-11-08 12:09:22 +0200

    greedy, *1100

---
+++ Greedy/A. Indirect Sort.cpp
@@ -0,0 +1,33 @@
+// Problem Name: A. Indirect Sort
+// Problem Link: https://codeforces.com/contest/1750/problem/A
+// Language: CPP
+// Submission Link: https://codeforces.com/contest/1750/submission/180065321
+// User: Mahmoud_Gamal_
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+        ios_base :: sync_with_stdio(0),
+        cin.tie(0);
+
+
+        int T ; cin >> T;
+        while(T--)
+        {
+            int n , m ; cin >> n >> m;
+            for(int i = 0; i < n - 1;i++)
+            {
+                int x ; cin >> x;
+            }
+            cout << (m==1 ? "Yes\n" : "No\n");
+            
+        }
+
+
+	 return 0;
+
+}
