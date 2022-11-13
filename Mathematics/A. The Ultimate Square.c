d9573675948792feed1f9c497c9feaf46d7395ea
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-11-13 10:24:45 +0200

    math

---
+++ Mathematics/A. The Ultimate Square.c
@@ -0,0 +1,35 @@
+// Problem Name: A. The Ultimate Square
+// Problem Link: https://codeforces.com/contest/1748/problem/A
+// Language: C
+// Submission Link: https://codeforces.com/contest/1748/submission/180723545
+// User: Mahmoud_Gamal_
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+        ios_base :: sync_with_stdio(0),
+        cin.tie(0);
+
+        int T ; scanf("%d", &T);
+        while(T--)
+        {
+            int n;
+            scanf("%d", &n);
+            printf("%d\n", n / 2 + (n & 1));
+
+        }
+
+
+
+
+
+
+
+
+
+	 return 0;
+
+}
+
