692cf1cbf914f9e9b94f274f6424ecbbeffedd63
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-11-26 23:45:36 +0200

    greedy, *1300

---
+++ Brute Force/A. Doremy's Paint.cpp
@@ -0,0 +1,40 @@
+// Problem Name: A. Doremy's Paint
+// Problem Link: https://codeforces.com/contest/1764/problem/A
+// Language: CPP
+// Submission Link: https://codeforces.com/contest/1764/submission/182724493
+// User: Mahmoud_Gamal_
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void getAC()
+{
+              int n;
+              cin >> n;
+              for(int i = 0; i < n;i++)
+              {
+                  int ans ; cin >> ans;
+              }
+              cout << "1 " <<n<< '\n';
+
+}
+int main()
+{
+        ios_base :: sync_with_stdio(0),
+        cin.tie(0);
+
+
+
+        int T ; cin >> T;
+        while(T--)
+        {
+            getAC();
+        }
+
+
+
+
+	 return 0;
+
+}
