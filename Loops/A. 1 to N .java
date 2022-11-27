905ba544e3dd1e3432ad24a4a89e61346792608e
Mahmoud Hassanein <mahmoud.gamal.ahmed.hassanein@gmail.com>
2022-11-27 15:55:58 +0200

    loop, *800

---
+++ Loops/A. 1 to N .java
@@ -0,0 +1,22 @@
+// Problem Name: A. 1 to N
+// Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/A
+// Language: JAVA
+// Submission Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/submission/182912516
+// User: Mahmoud_Gamal_
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+        
+        Scanner in = new Scanner (System.in);
+        int n = in.nextInt();
+        for(int i = 1; i < n;i++)
+        {
+            System.out.println(i);
+        }
+        System.out.print(n);
+
+	}
+}
